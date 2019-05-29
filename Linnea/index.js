9dcb2ea6e3cea63f887be2141ca5636c4f399233
Linneafy <linneafylling@gmail.com>
2019-05-29 16:54:34 +0200

    IndexedDB - bugs: still not working

--- Linnea/index.js
+++ Linnea/index.js
@@ -10,36 +10,39 @@ function addText() {
     
 
 //open connection to database
-let request = window.indexedDB.open("infoDatabase", 2), 
-    textDB,
-    textTx,
-    textStore,
-    index;
+    let request = window.indexedDB.open("infoDatabase", 2), 
+        textDB,
+        textTx,
+        textStore,
+        index;
     
- //upgrade the database
-request.onupgradeneeded = function(event) {
-    let textDB = request.result,
+        //upgrade the database
+        request.onupgradeneeded = function(event) {
+        let textDB = request.result,
         
         
-    //titleDatabase    
-    titleStore = textDB.createObjectStore("titleStore", { 
+        //titleDatabase    
+        titleStore = textDB.createObjectStore("titleStore", { 
                     keyPath: "titleID", autoIncrement: true}), 
+            
+        titleIndex = titleStore.createIndex("title", { unique: false}), 
+            
+            
         
-    titleIndex = titlesStore.createIndex("title", { unique: false}), 
-        
-    //duedateDatabase    
-    duedateStore = textDB.createObjectStore("duedateStore", {  
+        //duedateDatabase    
+        duedateStore = textDB.createObjectStore("duedateStore", {  
                     keyPath: "duedateID", autoIncrement: true}), 
         
-    duedateIndex = duedateStore.createIndex("duedate", { unique: false}),
+        duedateIndex = duedateStore.createIndex("duedate", { unique: false}),
+            
         
         
-    //descriptionDatabase    
-    descriptionStore = textDB.createObjectStore("descriptionStore", { 
+        //descriptionDatabase    
+        descriptionStore = textDB.createObjectStore("descriptionStore", { 
                     keyPath: "descriptionID", autoIncrement: true}), 
         
-    descriptionIndex = descriptionStore.createIndex("description", { unique: false});
-};
+        descriptionIndex = descriptionStore.createIndex("description", { unique: false});
+        };
 
     //error handler on connection
     request.onerror = function(event) {
@@ -47,44 +50,45 @@ request.onupgradeneeded = function(event) {
     }
 
  
-    //success handler
-    request.onsuccess = function(event) {
-        console.log("Successfully added task to database");
-        textDB = request.result;
+        //success handler
+        request.onsuccess = function(event) {
+            console.log("Successfully added task to database");
+            textDB = request.result;
     
-        //define transaction, store and index 
-        //title
-        titleTx = textDB.transaction("titleStore", "readwrite");
-        titleStore = textDB.objectStore("titleStore");
-        titleIndex = titleStore.index("title");
+                //define transaction, store and index 
+                //title
+                titleTx = textDB.transaction("titleStore", "readwrite");
+                titleStore = textDB.objectStore("titleStore");
+                titleIndex = titleStore.index("title");
     
-        //duedate
-        duedateTx = textDB.transaction("duedateStore", "readwrite");
-        duedateStore = duedateTx.obejctStore("duedateStore");
-        duedateIndex = duedateStore.index("duedate");
+                //duedate
+                duedateTx = textDB.transaction("duedateStore", "readwrite");
+                duedateStore = duedateTx.obejctStore("duedateStore");
+                duedateIndex = duedateStore.index("duedate");
     
-        //description
-        descriptionTx = textDB.transaction("descriptionStore", "readwrite");
-        descriptionStore = descriptionTx.objectStore("descriptionStore");
-        descriptionIndex = descriptionStore.index("description");
+                //description
+                descriptionTx = textDB.transaction("descriptionStore", "readwrite");
+                descriptionStore = descriptionTx.objectStore("descriptionStore");
+                descriptionIndex = descriptionStore.index("description");
+            
         
         //error handler on result of the request
         textDB.onerror = function(event) {
             console.log("ERROR " + e.target.errorCode);
         }
         
-        //add new variables to the database
-        let newTitle = titleStore.add({
-        title: titleInput
-        });
+            //add new variables to the database
+            let newTitle = titleStore.add({
+            title: titleInput
+            });
     
-        let newDuedate = duedateStore.add({
-        duedate: duedateInput
-        });
+            let newDuedate = duedateStore.add({
+            duedate: duedateInput
+            });
     
-        let newDescription = descriptionStore.add({
-        description: descriptionInput
-        });
+            let newDescription = descriptionStore.add({
+            description: descriptionInput
+            });
         
         
         //success handler on adding new info to the database handler
@@ -102,6 +106,7 @@ request.onupgradeneeded = function(event) {
         }
     
 function listText() {
+      
     //open connection to database 
     let request = window.indexedDB.open("infoDatabase", 2), 
         textDB,
@@ -139,6 +144,7 @@ function listText() {
         textDB.onerror = function(event) {
             console.log("ERROR " + e.target.errorCode);
         }
+    
         
         //variable for counting objects in the index
         let amountOfText = titleIndex.count();
@@ -152,7 +158,12 @@ function listText() {
         amountOfText.onsuccess = function() {
         
             for (i = 0; i < amountOfText.result+1; i++) {
-                let getText = titleStore.get(i);
+                let getText = titleStore.get[1];
+                
+                let titleContainer = document.getElementById("new-title");
+                let duedateContainer = document.getElementById("new-duedate");
+                let descriptionContainer = document.getElementById("new-description");
+            
                 
                 getText.onerror = function() {
                     console.log("There was an error looping through the text");
@@ -160,15 +171,23 @@ function listText() {
                 
                 getText.onsuccess = function() {
                     console.log(getText.result);
+                    titleContainer.innerHTML = JSON.stringify(getText.result);
                 }
             }
         }
     }
 }
+            
+function handleForm(e) {
+    e.preventDefault();
+}
     
    
-
+//handleForm is defined in 
+//form handler to prevent page from reloading
 var addTextForm = document.getElementById("addTextForm");
-        
-addTextForm.addEventListener('submit', handleForm);
+var textForm = document.getElementById("listText");
 
+//to prevent page from reloading
+addTextForm.addEventListener("submit", handleForm);
+textForm.addEventListener("submit", handleForm);
--- Linnea/indexedDB.js
+++ Linnea/indexedDB.js
@@ -1,2 +1,23 @@
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDb || window.msIndexedDB;
 
+
+function createDB() {
+    
+    let request = indexedDB.open("infoDatabase")
+
+        //on upgrade needed 
+        request.onupgradeneeded = function(e) {
+            alert("upgrade is called")
+
+        }
+
+        //on success
+        request.onsuccess = function(e) {
+            alert("success is called")
+        }
+
+        request.onerror = function(e) {
+            alert("error!")
+        }
+    }
+
