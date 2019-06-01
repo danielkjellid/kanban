050acc0ba25f4b04f1c32a35e65932b7f5a82119
danielkjellid <daniel.kjellid@gmail.com>
2019-06-01 17:12:07 +0200

    Small changes

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,4 +1,38 @@
 //function to load different functions onload.
+let members = [
+    {
+        id: 1,
+        memberFullName: "Daniel Kjellid",
+        memberInitials: "DK"
+    },
+    {
+        id: 2,
+        memberFullName: "Kainat Zahoor",
+        memberInitials: "KZ"
+    },
+    {
+        id: 3,
+        memberFullName: "Linnea S. Fylling",
+        memberInitials: "LF"
+    },
+    {
+        id: 4,
+        memberFullName: "Magomed Derbtichev",
+        memberInitials: "MD"
+    },
+    {
+        id: 5,
+        memberFullName: "Sultan Khan",
+        memberInitials: "SK"
+    }
+];
+
+let tags = [
+    {
+        //tags info
+    }
+];
+
 function onLoad() {
     /*listMembers();
     listTasks();
